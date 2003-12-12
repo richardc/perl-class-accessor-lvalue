1f122e1758e516270ff2d7677f54e837c7327498
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-12-12 11:42:57 +0000

    docs
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Accessor-Lvalue@1883 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Class/Accessor/Lvalue.pm
+++ lib/Class/Accessor/Lvalue.pm
@@ -6,3 +6,31 @@ our $VERSION = '0.01';
 croak "Class::Accessor::Lvalue is just a dummy package, you want Class::Accessor::Lvalue::Fast";
 
 1;
+__END__
+
+=head1 NAME
+
+Class::Accessor::Lvalue - create Lvalue accessors
+
+=head1 DESCRIPTION
+
+This module is non-functional, see L<Class::Accessor::Lvalue::Fast>,
+distributed with this one) for one that is and a discussion as to why
+this is.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Class::Accessor>, L<Class::Accessor::Lvalue::Fast>
+
+=cut
--- lib/Class/Accessor/Lvalue/Fast.pm
+++ lib/Class/Accessor/Lvalue/Fast.pm
@@ -3,13 +3,6 @@ package Class::Accessor::Lvalue::Fast;
 use base qw(Class::Accessor::Fast);
 use Want;
 
-=head1 NOTE
-
-This only comes in a ::Fast variant, which is to say, doesn't go via
-->get and ->set
-
-=cut
-
 sub make_accessor {
     my ($class, $field) = @_;
 
@@ -57,3 +50,48 @@ sub make_wo_accessor {
 1;
 __END__
 
+
+=head1 NAME
+
+Class::Accessor::Lvalue::Fast - create simplified Lvalue accessors
+
+=head1 SYNOPSIS
+
+ package Foo;
+ use base qw( Class::Accessor::Lvalue::Fast );
+ __PACKAGE__->mk_accessors(qw( bar ))
+
+ my $foo = Foo->new;
+ $foo->bar = 42;
+ print $foo->bar; # prints 42
+
+
+=head1 DESCRIPTION
+
+This module subclassess L<Class::Accessor::Fast> in order to provide
+lvalue accessors.
+
+=head1 IMPLEMENTATION NOTES
+
+You may have noted that this accessor-maker only comes in a ::Fast
+variant.  This is because the non-fast variant would use an
+indirection through ->get and ->set methods - I couldn't at the time
+of writing see a way to make that lvaluable.
+
+If you have a suggestion for that, I'd like to hear from you.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp.  All Rights Reserved.
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Class::Accessor::Fast>, L<Want>
+
+=cut
