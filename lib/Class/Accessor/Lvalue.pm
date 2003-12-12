40e94285048f497bc76a3fdeecd1d74089bd2d7d
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-12-12 11:03:02 +0000

    rename a little
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Accessor-Lvalue@1882 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Class/Accessor/Lvalue.pm
@@ -0,0 +1,8 @@
+use strict;
+package Class::Accessor::Lvalue;
+use Carp qw(croak);
+our $VERSION = '0.01';
+
+croak "Class::Accessor::Lvalue is just a dummy package, you want Class::Accessor::Lvalue::Fast";
+
+1;
--- lib/Class/Accessor/Lvalue/Fast.pm
+++ lib/Class/Accessor/Lvalue/Fast.pm
@@ -2,7 +2,6 @@ use strict;
 package Class::Accessor::Lvalue::Fast;
 use base qw(Class::Accessor::Fast);
 use Want;
-our $VERSION = '0.01';
 
 =head1 NOTE
 
