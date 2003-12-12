25c77e3dab531a6405116a2507f56d940888a2f3
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-12-12 23:44:50 +0000

    I love it when a plan comes together
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Accessor-Lvalue@1889 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Class/Accessor/Lvalue.pm
+++ lib/Class/Accessor/Lvalue.pm
@@ -1,9 +1,72 @@
 use strict;
 package Class::Accessor::Lvalue;
 use base qw( Class::Accessor );
-use Carp qw(croak);
-use Want;
+use Scalar::Util qw(weaken);
+use Want qw( want );
 our $VERSION = '0.01';
+
+tie my $rw, "Class::Accessor::Lvalue::Tied";
+
+sub _tied_accessor :lvalue {
+    $Class::Accessor::Lvalue::Tied::field = shift;
+    $Class::Accessor::Lvalue::Tied::self  = shift;
+    weaken $Class::Accessor::Lvalue::Tied::self;
+    $rw;
+}
+
+sub make_accessor {
+    my ($class, $field) = @_;
+
+    return sub :lvalue {
+        _tied_accessor( $field, @_ );
+    };
+}
+
+sub make_ro_accessor {
+    my ($class, $field) = @_;
+
+    return sub :lvalue {
+        my $self = shift;
+        if (want 'LVALUE') {
+            my $caller = caller;
+            require Carp;
+            Carp::croak("'$caller' cannot alter the value of '$field' on ".
+                          "objects of class '$class'");
+        }
+        _tied_accessor( $field, $self, @_ );
+    };
+}
+
+
+sub make_wo_accessor {
+    my($class, $field) = @_;
+
+    return sub :lvalue {
+        my $self = shift;
+        unless (want 'LVALUE') {
+            my $caller = caller;
+            require Carp;
+            Carp::croak("'$caller' cannot access the value of '$field' on ".
+                          "objects of class '$class'");
+        }
+        _tied_accessor( $field, $self, @_ );
+    };
+}
+
+
+package Class::Accessor::Lvalue::Tied;
+sub TIESCALAR { bless {} }
+
+sub STORE {
+    shift;
+    our ($self, $field);
+    $self->set( $field, @_ );
+}
+
+sub FETCH {
+    our ($self, $field);
+    $self->get( $field );
+}
 
 1;
 __END__
